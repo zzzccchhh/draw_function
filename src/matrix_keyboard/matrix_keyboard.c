b632215465a818fd7b7478fa924880f297fccf64
zzz <982026486@qq.com>
2026-05-11 23:29:10 +0800

    9

---
+++ src/matrix_keyboard/matrix_keyboard.c
@@ -0,0 +1,379 @@
+#include "matrix_keyboard.h"
+#include "oled_ssd1306.h"
+#include "oled_hal.h"
+#include <stdio.h>
+
+//=================================================== 宏定义 ===================================================
+#define MATRIX_KEY_NONE   0xFF
+
+//=================================================== 静态常量 ===================================================
+// Row 引脚数组
+static const gpio_pin_enum matrix_row_pin[MATRIX_ROW_NUM] = {
+    MATRIX_ROW_0, MATRIX_ROW_1, MATRIX_ROW_2, MATRIX_ROW_3
+};
+
+// Col 引脚数组
+static const gpio_pin_enum matrix_col_pin[MATRIX_COL_NUM] = {
+    MATRIX_COL_0, MATRIX_COL_1, MATRIX_COL_2, MATRIX_COL_3
+};
+
+// 按键值映射表 (短按输出 1-16)
+static const uint8 short_press_map[MATRIX_ROW_NUM][MATRIX_COL_NUM] = {
+    {1, 2, 3,  4},
+    {5, 6, 7,  8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16}
+};
+
+//=================================================== 静态变量 ===================================================
+static uint32 scan_period = MATRIX_SCAN_PERIOD;
+static matrix_key_state_enum current_state = MATRIX_IDLE;
+
+static uint8 current_row = 0;
+static uint8 current_col = 0;
+static uint8 current_key_index = 0;
+static uint32 press_time_counter = 0;
+
+static uint8 key_value = 0;
+static uint8 long_press_triggered = 0;
+static uint8 debounce_count = 0;
+
+static matrix_key_callback user_callback = NULL;
+
+static matrix_key_info_struct key_info = {0};
+
+//=================================================== 内部函数 ===================================================
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数名称     读取所有列的状态，返回列掩码
+//-------------------------------------------------------------------------------------------------------------------
+static uint8 matrix_read_cols(void)
+{
+    uint8 col_mask = 0;
+    uint8 i;
+    for(i = 0; i < MATRIX_COL_NUM; i++)
+    {
+        if(gpio_get_level(matrix_col_pin[i]) == GPIO_LOW)
+        {
+            col_mask |= (1 << i);
+        }
+    }
+    return col_mask;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数名称     扫描单行，返回按下的列掩码
+//-------------------------------------------------------------------------------------------------------------------
+static uint8 matrix_scan_row(uint8 row)
+{
+    uint8 i;
+
+    // 先设置所有行为高电平
+    for(i = 0; i < MATRIX_ROW_NUM; i++)
+    {
+        gpio_set_level(matrix_row_pin[i], GPIO_HIGH);
+    }
+
+    // 再将当前行设为低电平
+    gpio_set_level(matrix_row_pin[row], GPIO_LOW);
+
+    // 等待信号稳定
+    system_delay_us(50);
+
+    // 读取列状态
+    return matrix_read_cols();
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数名称     扫描整个矩阵，返回按下的键位置
+// 返回值: MATRIX_KEY_NONE 表示无按键按下，否则返回 (row << 4) | col
+//-------------------------------------------------------------------------------------------------------------------
+static uint8 matrix_scan_all(void)
+{
+    uint8 row, col;
+    uint8 col_mask;
+
+    for(row = 0; row < MATRIX_ROW_NUM; row++)
+    {
+        col_mask = matrix_scan_row(row);
+        if(col_mask != 0)
+        {
+            // 找到按下的键，只取最低位为1的列
+            for(col = 0; col < MATRIX_COL_NUM; col++)
+            {
+                if(col_mask & (1 << col))
+                {
+                    return (row << 4) | col;
+                }
+            }
+        }
+    }
+
+    return MATRIX_KEY_NONE;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数名称     将 row/col 转换为按键值
+//-------------------------------------------------------------------------------------------------------------------
+static uint8 matrix_get_key_value(uint8 row, uint8 col, uint8 is_long_press)
+{
+    uint8 index = short_press_map[row][col];
+
+    if(is_long_press)
+    {
+        return 'A' + index - 1;
+    }
+
+    return index;
+}
+
+//=================================================== 状态机实现 ===================================================
+
+void matrix_keyboard_scanner(void)
+{
+    uint8 key_position;
+
+    switch(current_state)
+    {
+        //-----------------------------------
+        case MATRIX_IDLE:
+        //-----------------------------------
+            key_position = matrix_scan_all();
+            if(key_position != MATRIX_KEY_NONE)
+            {
+                current_row = key_position >> 4;
+                current_col = key_position & 0x0F;
+                debounce_count = 1;
+                current_state = MATRIX_DEBOUNCE;
+            }
+            break;
+
+        //-----------------------------------
+        case MATRIX_DEBOUNCE:
+        //-----------------------------------
+            key_position = matrix_scan_all();
+            if(key_position != MATRIX_KEY_NONE &&
+               (key_position >> 4) == current_row &&
+               (key_position & 0x0F) == current_col)
+            {
+                debounce_count++;
+                if(debounce_count >= (MATRIX_DEBOUNCE_TIME / scan_period))
+                {
+                    current_key_index = short_press_map[current_row][current_col];
+                    press_time_counter = 0;
+                    long_press_triggered = 0;
+                    key_value = 0;
+                    current_state = MATRIX_PRESSED;
+
+                    if(user_callback)
+                    {
+                        user_callback(current_key_index, MATRIX_EVENT_SHORT_PRESS, current_key_index);
+                    }
+                }
+            }
+            else
+            {
+                current_state = MATRIX_IDLE;
+                debounce_count = 0;
+            }
+            break;
+
+        //-----------------------------------
+        case MATRIX_PRESSED:
+        //-----------------------------------
+            key_position = matrix_scan_all();
+            if(key_position != MATRIX_KEY_NONE &&
+               (key_position >> 4) == current_row &&
+               (key_position & 0x0F) == current_col)
+            {
+                press_time_counter++;
+
+                if(!long_press_triggered &&
+                   press_time_counter >= (MATRIX_LONG_PRESS_TIME / scan_period))
+                {
+                    long_press_triggered = 1;
+                    key_value = matrix_get_key_value(current_row, current_col, 1);
+                    current_state = MATRIX_LONG_PRESS;
+
+                    matrix_keyboard_display_info(key_value);
+
+                    if(user_callback)
+                    {
+                        user_callback(current_key_index, MATRIX_EVENT_LONG_PRESS, key_value);
+                    }
+                }
+            }
+            else
+            {
+                if(!long_press_triggered)
+                {
+                    key_value = current_key_index;
+                    matrix_keyboard_display_info(key_value);
+                }
+
+                if(user_callback)
+                {
+                    user_callback(current_key_index, MATRIX_EVENT_RELEASE, key_value);
+                }
+
+                current_state = MATRIX_RELEASE;
+            }
+            break;
+
+        //-----------------------------------
+        case MATRIX_LONG_PRESS:
+        //-----------------------------------
+            key_position = matrix_scan_all();
+            if(key_position != MATRIX_KEY_NONE &&
+               (key_position >> 4) == current_row &&
+               (key_position & 0x0F) == current_col)
+            {
+                // 保持长按状态
+            }
+            else
+            {
+                key_value = 0;
+                long_press_triggered = 0;
+                current_state = MATRIX_RELEASE;
+            }
+            break;
+
+        //-----------------------------------
+        case MATRIX_RELEASE:
+        //-----------------------------------
+            key_position = matrix_scan_all();
+            if(key_position == MATRIX_KEY_NONE)
+            {
+                current_state = MATRIX_IDLE;
+                key_value = 0;
+                long_press_triggered = 0;
+            }
+            break;
+
+        //-----------------------------------
+        default:
+        //-----------------------------------
+            current_state = MATRIX_IDLE;
+            break;
+    }
+}
+
+//=================================================== 外部API实现 ===================================================
+
+void matrix_keyboard_init(uint32 scan_period_ms)
+{
+    uint8 i;
+
+    for(i = 0; i < MATRIX_ROW_NUM; i++)
+    {
+        gpio_init(matrix_row_pin[i], GPO, GPIO_HIGH, GPO_PUSH_PULL);
+    }
+
+    for(i = 0; i < MATRIX_COL_NUM; i++)
+    {
+        gpio_init(matrix_col_pin[i], GPI, GPIO_HIGH, GPI_PULL_UP);
+    }
+
+    scan_period = scan_period_ms;
+    current_state = MATRIX_IDLE;
+    key_value = 0;
+    long_press_triggered = 0;
+    debounce_count = 0;
+    press_time_counter = 0;
+    current_row = 0;
+    current_col = 0;
+    current_key_index = 0;
+
+    key_info.state = MATRIX_IDLE;
+    key_info.row = 0;
+    key_info.col = 0;
+    key_info.index = 0;
+    key_info.press_time = 0;
+    key_info.event = MATRIX_EVENT_NONE;
+}
+
+void matrix_keyboard_set_callback(matrix_key_callback callback)
+{
+    user_callback = callback;
+}
+
+uint8 matrix_keyboard_get_value(void)
+{
+    return key_value;
+}
+
+void matrix_keyboard_clear_event(void)
+{
+    key_value = 0;
+}
+
+matrix_key_info_struct matrix_keyboard_get_info(void)
+{
+    matrix_key_info_struct info;
+
+    info.row = current_row;
+    info.col = current_col;
+    info.index = current_key_index;
+    info.state = current_state;
+    info.press_time = press_time_counter * scan_period;
+
+    if(long_press_triggered)
+    {
+        info.event = MATRIX_EVENT_LONG_PRESS;
+    }
+    else if(current_state == MATRIX_PRESSED || current_state == MATRIX_RELEASE)
+    {
+        info.event = MATRIX_EVENT_SHORT_PRESS;
+    }
+    else
+    {
+        info.event = MATRIX_EVENT_NONE;
+    }
+
+    return info;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+// 函数名称     在OLED上显示按键信息
+//-------------------------------------------------------------------------------------------------------------------
+void matrix_keyboard_display_info(uint8 key_value)
+{
+    ssd1306_clearScreen();
+    oled_setTextColor(WHITE, BLACK);
+
+    if(key_value >= 'A' && key_value <= 'P')
+    {
+        // 长按：显示字母
+        oled_setTextSize(2);
+        oled_drawText(0, 0, "P:");
+        oled_drawChar(20, 0, key_value, WHITE, BLACK, 2);
+        oled_drawText(64, 0, "C:");
+        oled_drawChar(84, 0, key_value, WHITE, BLACK, 2);
+    }
+    else if(key_value >= 1 && key_value <= 16)
+    {
+        // 短按：显示数字
+        oled_setTextSize(2);
+        oled_drawText(0, 0, "P:");
+        oled_displayInt(20, 0, key_value);
+        oled_drawText(64, 0, "C:");
+        oled_displayInt(84, 0, key_value);
+    }
+    else
+    {
+        oled_setTextSize(2);
+        oled_drawText(40, 20, "No Key");
+        ssd1306_updateScreen();
+        return;
+    }
+
+    // 显示键位映射表
+    oled_setTextSize(1);
+    oled_drawText(0, 30, "1 2 3 4");
+    oled_drawText(0, 42, "5 6 7 8");
+    oled_drawText(64, 30, "9 10 11 12");
+    oled_drawText(64, 42, "13 14 15 16");
+    oled_drawText(0, 54, "Long: A-P");
+
+    ssd1306_updateScreen();
+}
---
+++ src/matrix_keyboard/matrix_keyboard.h
@@ -0,0 +1,84 @@
+#ifndef __MATRIX_KEYBOARD_H__
+#define __MATRIX_KEYBOARD_H__
+
+#include "zf_common_headfile.h"
+
+//=================================================== 硬件定义 ===================================================
+// Row 引脚定义 (输出，从上到下)
+#define MATRIX_ROW_0    B7                     // p17 → Row 0
+#define MATRIX_ROW_1    E0                     // p16 → Row 1
+#define MATRIX_ROW_2    E1                     // p15 → Row 2
+#define MATRIX_ROW_3    E2                     // p14 → Row 3
+
+// Col 引脚定义 (输入，从左到右)
+#define MATRIX_COL_0    E3                     // p13 → Col 0
+#define MATRIX_COL_1    E4                     // p12 → Col 1
+#define MATRIX_COL_2    E5                     // p11 → Col 2
+#define MATRIX_COL_3    E6                     // p10 → Col 3
+
+//=================================================== 参数配置 ===================================================
+#define MATRIX_ROW_NUM              (4)
+#define MATRIX_COL_NUM              (4)
+#define MATRIX_KEY_NUM              (MATRIX_ROW_NUM * MATRIX_COL_NUM)   // 16
+
+#define MATRIX_DEBOUNCE_TIME        (10)        // 消抖时间 (ms)
+#define MATRIX_LONG_PRESS_TIME      (1000)      // 长按判定时间 (ms)
+#define MATRIX_SCAN_PERIOD          (10)        // 默认扫描周期 (ms)
+
+//=================================================== 状态机状态枚举 ===================================================
+typedef enum
+{
+    MATRIX_IDLE = 0,                           // 空闲状态，无按键按下
+    MATRIX_DEBOUNCE,                            // 消抖中
+    MATRIX_PRESSED,                             // 按键确认按下
+    MATRIX_LONG_PRESS,                          // 长按触发
+    MATRIX_RELEASE                              // 按键释放
+} matrix_key_state_enum;
+
+//=================================================== 按键事件枚举 ===================================================
+typedef enum
+{
+    MATRIX_EVENT_NONE = 0,
+    MATRIX_EVENT_SHORT_PRESS,                   // 短按事件
+    MATRIX_EVENT_LONG_PRESS,                     // 长按事件
+    MATRIX_EVENT_RELEASE                        // 释放事件
+} matrix_key_event_enum;
+
+//=================================================== 按键信息结构体 ===================================================
+typedef struct
+{
+    uint8 row;                                  // 按键所在行 (0-3)
+    uint8 col;                                  // 按键所在列 (0-3)
+    uint8 index;                                // 按键编号 (1-16)
+    matrix_key_state_enum state;                // 当前状态
+    uint32 press_time;                          // 按下持续时间 (ms)
+    matrix_key_event_enum event;                // 触发的事件
+} matrix_key_info_struct;
+
+//=================================================== 事件回调函数类型 ===================================================
+typedef void (*matrix_key_callback)(uint8 key_index, matrix_key_event_enum event, uint8 key_value);
+
+//=================================================== API 函数 ===================================================
+// 初始化矩阵键盘
+void matrix_keyboard_init(uint32 scan_period_ms);
+
+// 设置按键事件回调
+void matrix_keyboard_set_callback(matrix_key_callback callback);
+
+// 扫描函数（需在主循环或定时器中调用）
+void matrix_keyboard_scanner(void);
+
+// 获取当前按下的按键值
+// 返回值: 0 表示无按键, 1-16 表示短按, 'A'-'P' 表示长按
+uint8 matrix_keyboard_get_value(void);
+
+// 清除按键事件
+void matrix_keyboard_clear_event(void);
+
+// 在OLED上显示按键功能
+void matrix_keyboard_display_info(uint8 key_value);
+
+// 获取当前按键信息
+matrix_key_info_struct matrix_keyboard_get_info(void);
+
+#endif
