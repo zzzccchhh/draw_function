d0a69459fa644c9d342eda551a44ca011763efce
zzz <982026486@qq.com>
2026-05-12 23:59:43 +0800

    26

--- src/function_plotter.c
+++ src/function_plotter.c
@@ -4,7 +4,6 @@
 #include "uart1.h"
 #include <math.h>
 #include <stdio.h>
-//12345678903333
 
 const function_preset_t presets[] = {
     {FUNC_QUADRATIC,   "y=x^2/50",          {0.02f,  0.0f,   0.0f,  0.0f}},
